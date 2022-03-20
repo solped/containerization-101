9d33cfb5bc50f74ff9f5e38fae70ffb83e726eb6
worasit <worasit.daimongkol@thoughtworks.com>
2022-03-20 21:02:16 +0700

    added DB migration with seeds

---
+++ server/seeders/20220320135853-tasks-demo.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        await queryInterface.bulkInsert('Tasks', [{
+            text: 'This is the first task',
+            isDone: false,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }], {});
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('Tasks', null, {});
+    }
+};
--- server/server.js
+++ server/server.js
@@ -2,11 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const ip = require("ip");
+const {Sequelize} = require('@sequelize/core');
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
+
+const sequelize = new Sequelize('postgres', 'admin', 'admin', {
+    // gimme postgres, please!
+    host: "localhost",
+    dialect: 'postgres',
+    port: 5432
+})
+
+
+sequelize.authenticate()
+    .then(r => console.log('Connection has been established successfully.'))
+    .catch(error => console.error('Unable to connect to the database:', error));
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
