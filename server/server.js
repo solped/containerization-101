1daf2aa8848286636a8f26cfa459a82ed52bd811
worasit <worasit.daimongkol@thoughtworks.com>
2022-03-24 19:40:13 +0700

    [Em,Mart] Added POST, PUT and DELETE endpoints on server

--- server/server.js
+++ server/server.js
@@ -19,6 +19,14 @@ app.get('/healthcheck', (req, res) => {
     res.send(`Server is available at ip: ${ip.address()}`);
 });
 
+app.get('/tasks', (req, res) => {
+    model.sequelize.model('Task').findAll({}).then(result => {
+        res.json(result)
+    }).catch(err => {
+        console.log(err)
+    })
+});
+
 app.post('/tasks', (req, res) => {
     model.sequelize.model('Task').create({
         text: req.body.text,
@@ -28,12 +36,24 @@ app.post('/tasks', (req, res) => {
     );
 });
 
-app.get('/tasks', (req, res) => {
-    model.sequelize.model('Task').findAll({}).then(result => {
-        res.json(result)
-    }).catch(err => {
-        console.log(err)
-    })
+app.put('/tasks/:id', (req, res) => {
+    model.sequelize.model('Task').update({isDone: req.body.isDone}, {
+        where: {
+            id: req.params.id,
+        }
+    }).then(result => {
+        res.json(`The task ${req.params.id} has been updated`)
+    }).catch(err => console.log(err))
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    model.sequelize.model('Task').destroy({
+        where: {
+            id: req.params.id,
+        }
+    }).then(result => {
+        res.json(`The task ${req.params.id} has been deleted`)
+    }).catch(err => console.log(err))
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
